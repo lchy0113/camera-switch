d56ef926ba385af31b058bdfaba8c85a053a130e
changyong Lee <lchy0113@gmail.com>
2023-03-21 14:13:41 +0900

    chore : Update build.gradle
    Change-Id: If9e368d388fd7c8e434cf87389462785c1f04d1c

--- app/src/androidTest/java/com/richgold/cameraactivity/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/richgold/cameraactivity/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.wallpad.cameraactivity", appContext.getPackageName());
+        assertEquals("com.richgold.cameraactivity", appContext.getPackageName());
     }
 }
